152665069261b1f49ca4762c8209f2a67cda8ce0
JeallyRabbit <119430599+JeallyRabbit@users.noreply.github.com>
2023-09-09 15:04:27 +0200

    Add files via upload

--- role.transporter.js
+++ role.transporter.js
@@ -7,7 +7,7 @@ var roleTransporter = {//transfer energy grom containers to extensions and spawn
     {
         var deposit=getClosestEnergyDeposit(creep);
         //creep.say(creep.store.getFreeCapacity());
-        if(creep.store.getFreeCapacity()>0)
+        if(creep.store.getFreeCapacity()>0 && deposit)
         {
             withdraw_amount=Math.min(creep.store[RESOURCE_ENERGY].getFreeCapacity, deposit.store[RESOURCE_ENERGY]);
             if(creep.withdraw(deposit,RESOURCE_ENERGY,withdraw_amount)==ERR_NOT_IN_RANGE )
@@ -17,14 +17,19 @@ var roleTransporter = {//transfer energy grom containers to extensions and spawn
         }
         else
         {
-            creep.say("T");
+            //creep.say("T");
             var storage=creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return structure.structureType == STRUCTURE_STORAGE
                     && structure.store[RESOURCE_ENERGY]<20000;
                 }
             });
-            
+            storage+=creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return structure.structureType == STRUCTURE_TOWER
+                    && structure.store[RESOURCE_ENERGY]<20000;
+                }
+            });
             if(storage.length>0)
             {
                 //creep.say(storage[0].store[RESOURCE_ENERGY]);
