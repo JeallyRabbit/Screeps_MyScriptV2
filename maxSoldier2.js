bc932fd53be199bf6040f5de67f30482a9b2f13b
JeallyRabbit <karczewskiolek5@gmail.com>
2024-09-18 13:49:30 +0200

    as

--- maxSoldier2.js
+++ maxSoldier2.js
@@ -1,12 +1,12 @@
 function maxSoldier2(cap)// return array with max possible work parts for hauler
 {
     var parts=[];
-    if(cap>2280)
+    if(cap>3120)
     {
-        cap=2280;
+        cap=3120;
     }
 
-    for(let i=0;i<Math.floor(cap/140);i++)
+    for(let i=0;i<Math.floor(cap/260);i++)
     {
         parts.push(MOVE);
         parts.push(MOVE);
