3bb52897687d0fa4410b0ac7203e7c8a48be712d
aleks.karczewski <aleks.karczewski@student.put.poznan.pl>
2024-11-21 12:17:27 +0100

    asd

--- main.js
+++ main.js
@@ -728,27 +728,31 @@ module.exports.loop = function () {
                         //creep.suicide();
 
 
+                        if (creep.ticksToLive > 100 || creep.spawning) {
+                            if (creep.memory.harvesting_power == undefined) {
+                                const workParts = _.filter(creep.body, { type: WORK }).length;
+                                creep.memory.harvesting_power = workParts * 2;
+                            }
 
-                        if (creep.memory.harvesting_power == undefined) {
-                            const workParts = _.filter(creep.body, { type: WORK }).length;
-                            creep.memory.harvesting_power = workParts * 2;
-                        }
+                            for (let i = 0; i < spawn.memory.farming_rooms.length; i++) {
+                                if (spawn.memory.farming_rooms[i].name == creep.memory.target_room) {
+                                    spawn.memory.farming_rooms[i].harvesting_power += creep.memory.harvesting_power;
+                                    spawn.memory.farming_rooms[i].farmers++;
+                                    break;
+                                }
+                            }
 
-                        for (let i = 0; i < spawn.memory.farming_rooms.length; i++) {
-                            if (spawn.memory.farming_rooms[i].name == creep.memory.target_room) {
-                                spawn.memory.farming_rooms[i].harvesting_power += creep.memory.harvesting_power;
-                                spawn.memory.farming_rooms[i].farmers++;
-                                break;
+                            for (let i = 0; i < spawn.memory.farming_sources.length; i++) {
+                                if (spawn.memory.farming_sources[i].id == creep.memory.source_id) {
+                                    spawn.memory.farming_sources[i].harvesting_power += creep.memory.harvesting_power;
+                                    spawn.memory.farming_sources[i].farmers++;
+                                    break;
+                                }
                             }
                         }
 
-                        for (let i = 0; i < spawn.memory.farming_sources.length; i++) {
-                            if (spawn.memory.farming_sources[i].id == creep.memory.source_id) {
-                                spawn.memory.farming_sources[i].harvesting_power += creep.memory.harvesting_power;
-                                spawn.memory.farming_sources[i].farmers++;
-                                break;
-                            }
-                        }
+
+
                         if (creep.ticksToLive > creep.memory.time_to_sleep) {
                             creep.say('💤')
                             continue;
@@ -1138,7 +1142,7 @@ module.exports.loop = function () {
             spawn.room.visual.text("Time: " + Game.time, 20, 0, { color: '#fc03b6' })
 
             spawn.room.visual.text("Calculated final income/t: " + spawn.memory.total_calculated_income_per_tick, 44, 3, { color: 'lightblue' })
-            
+
             spawn.room.visual.text("mean used Cpu: " + Math.round(spawn.memory.mean_cpu * 100) / 100, 44, 5, { color: '#fc03b6' })
             spawn.room.visual.text("Progress/tick: " + Math.round((spawn.memory.progress_sum / spawn.memory.progress_counter) * 100) / 100,
                 44, 6, { color: 'lightblue' })
@@ -1221,9 +1225,9 @@ module.exports.loop = function () {
                     - spawn.room.memory.mean_total_repairers_energy
                 spawn.room.visual.text("final energy income/t: " + Math.round(spawn.room.memory.energy_income_t * 100) / 100, 41, 14, { color: 'lightblue' })
             }
-            spawn.room.visual.text((Game.time%step)+1 +"/"+step, 48, 4, {fill: 'transparent', font: '0.5'})
-            spawn.room.visual.rect(48,4.5,1,11,{fill: 'transparent', stroke: '#fff'})
-            spawn.room.visual.rect(48,4.5,1,(((Game.time%step)+1)/step)*11,{ stroke: '#fff'})
+            spawn.room.visual.text((Game.time % step) + 1 + "/" + step, 48, 4, { fill: 'transparent', font: '0.5' })
+            spawn.room.visual.rect(48, 4.5, 1, 11, { fill: 'transparent', stroke: '#fff' })
+            spawn.room.visual.rect(48, 4.5, 1, (((Game.time % step) + 1) / step) * 11, { stroke: '#fff' })
             //console.log(Game.time % step)
 
 
@@ -1659,7 +1663,7 @@ module.exports.loop = function () {
             else {
                 spawn.memory.mean_cpu_sum += spawn_end_cpu - spawn_start_cpu
             }
-            spawn.room.visual.text("used Cpu: " + Math.round(((spawn_end_cpu - spawn_start_cpu)) * 100) / 100 , 44, 4, { color: '#fc03b6' })
+            spawn.room.visual.text("used Cpu: " + Math.round(((spawn_end_cpu - spawn_start_cpu)) * 100) / 100, 44, 4, { color: '#fc03b6' })
 
             if (Game.time % step == 0) {
                 spawn.memory.mean_cpu = spawn.memory.mean_cpu_sum / step
